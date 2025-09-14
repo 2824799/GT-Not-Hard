b571560c12bbcf60702fdd036c8a68e8a4dbb967
Silvia <33190955+2824799@users.noreply.github.com>
2025-09-14 21:11:46 +0800

    Update ChaosZhuHaiRecipes.java

--- src/main/java/util/ChaosZhuHaiRecipes.java
+++ src/main/java/util/ChaosZhuHaiRecipes.java
@@ -16,24 +16,24 @@ import gregtech.api.util.GTUtility;
 public class ChaosZhuHaiRecipes {
 
     public static final RecipeMap<RecipeMapBackend> addChaosZhuHaiFisheryRecipes = RecipeMapBuilder
-        .of("Chaos of ZhuHai Fishery")
-        .maxIO(1, 1, 0, 0)
-        .minInputs(1, 0)
-        .disableOptimize()
-        .build();
+        ./("Chaos of ZhuHai Fishery")
+        。maxIO(1, 1, 0, 0)
+        。minInputs(1, 0)
+//        .disableOptimize()
+        。build();
 
     public static void addChaosZhuHaiFisheryRecipes() {
         GTValues.RA.stdBuilder()
-            .itemInputs(GTUtility.getIntegratedCircuit(1))
-            .itemOutputs(setStackSize(getModItem(Minecraft.ID, "dye", 1), 8192))
-            .duration(5 * SECONDS)
-            .eut(RECIPE_LV)
-            .addTo(addChaosZhuHaiFisheryRecipes);
+            。itemInputs(GTUtility。getIntegratedCircuit(1))
+            。itemOutputs(setStackSize(getModItem(Minecraft.ID, "dye"， 1)， 8192))
+            。duration(5 * SECONDS)
+            。eut(RECIPE_LV)
+            。addTo(addChaosZhuHaiFisheryRecipes);
 
         GTValues.RA.stdBuilder()
-            .itemInputs(GTUtility.getIntegratedCircuit(2))
-            .itemOutputs(setStackSize(getModItem(Minecraft.ID, "fish", 1L), 8192))
-            .duration(5 * SECONDS)
+            。itemInputs(GTUtility。getIntegratedCircuit(2))
+            。itemOutputs(setStackSize(getModItem(Minecraft.ID, "fish", 1L), 8192))
+            。duration(5 * SECONDS)
             .eut(RECIPE_LV)
             .addTo(addChaosZhuHaiFisheryRecipes);
 
